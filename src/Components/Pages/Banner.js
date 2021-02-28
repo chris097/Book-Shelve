05a0dfcba09efc476e2273e4b328b97349659fc1
chris097 <chrisfidel.international@gmail.com>
2021-02-28 13:09:10 +0100

    fix bug: onclick event

--- src/Components/Pages/Banner.js
+++ src/Components/Pages/Banner.js
@@ -6,16 +6,20 @@ import { Header } from "../Header/Header";
 
 
 export const Banner = ({getBookModal, book, getBookId, removeBookId }) => {
-    const [ mobSidebar, setMobSidebar ] = useState([])
+    const [ mobSidebar, setMobSidebar ] = useState(false)
+    const [showSidebar, setShowSidebar ] = useState(true)
+
     const getSidebarMobile = () => {
-        console.log('something was clicked...')
-        setMobSidebar(<MobileSidebar />)
+        // console.log('something was clicked...')
+        setMobSidebar(!mobSidebar)
+        setShowSidebar(!showSidebar)
     }
+    
     return(
         <div className="flex overflow-x-hidden">
             <Header getBookModal={getBookModal} book={book} getSidebarMobile={getSidebarMobile} />
             <Sidebar />
-            {mobSidebar}
+            {!showSidebar ? <MobileSidebar /> : showSidebar}
             <MainWrapper book={book} getBookModal={getBookModal} getBookId={getBookId} removeBookId={removeBookId}  />
         </div>
     )
