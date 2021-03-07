dad7709862679be4545a55de788ffd0bfe3fa3a3
chris097 <chrisfidel.international@gmail.com>
2021-03-07 07:10:24 +0100

    fixing bug

--- src/Components/Modal/Details.js
+++ src/Components/Modal/Details.js
@@ -42,10 +42,10 @@ export const BookDetails = ({
                     </div>
                 </div>
 
-                <div className="md:px-8 px-3 mt-2">
+                <div className="md:px-8 px-3 mt-4">
                     <div className="flex">
                     <div>32 comments</div>
-                    <div><svg className="w-6 ml-14 mt-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <div><svg className="w-6 ml-36 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                   </svg></div>
                     </div>
